f5e9f473bd33e25d49e00e0e1f93b7ee2dd1adf8
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-03-23 17:19:36 +0100

    Add 'post' Meteor method to be able to do things like validation, adding timestamp to hotels and tying a user to a hotel. Add requireLogin route to ensure users can only add hotels when they are logged in.

--- collections/hotels.js
+++ collections/hotels.js
@@ -1,1 +1,31 @@
 Hotels = new Meteor.Collection('hotels');
+
+Meteor.methods({
+  post: function(hotelAttributes)  {
+    var user = Meteor.user(), hotelWithSameWebsite = Hotels.findOne({website: hotelAttributes.website});
+
+    // ensure the user is logged in
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to post new stories");
+
+    // ensure the hotel has a name
+    if (!hotelAttributes.name)
+      throw new Meteor.Error(422, 'Please fill in a name');
+
+    // check that there are no previous hotels with the same website
+    if (hotelAttributes.website && hotelWithSameWebsite)  {
+      throw new Meteor.Error(302, 'This hotel has already been posted', hotelWithSameWebsite._id);
+    }
+
+    // pick out the whitelisted keys
+    var hotel = _.extend(_.pick(hotelAttributes, 'website', 'name', 'description'), {
+      userId: user._id,
+      manager: user.emails[0].address,
+      listed: new Date().getTime()
+    });
+
+    var hotelId = Hotels.insert(hotel);
+
+    return hotelId;
+  }
+});
