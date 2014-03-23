a6cf4d678d990af9bfd4bd1ec28c2d243688b3f6
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-03-23 19:32:00 +0100

    Templates and helpers for editing hotels.

---
+++ client/views/hotels/edit_hotel.js
@@ -0,0 +1,31 @@
+Template.editHotel.events({
+  'submit form': function(e)  {
+    e.preventDefault();
+
+    var currentHotelId = this._id;
+
+    var hotelProperties = {
+      website: $(e.target).find('[name=website]').val(),
+      name: $(e.target).find('[name=name]').val()
+    }
+
+    Hotels.update(currentHotelId, {$set: hotelProperties}, function (error) {
+      if (error)  {
+        // display the error to the user
+        alert(error.reason);
+      } else  {
+        Router.go('hotelPage', {_id: currentHotelId});
+      }
+    });
+  },
+
+  'click .delete': function(e)  {
+    e.preventDefault();
+
+    if (confirm("Delete this hotel?"))  {
+      var currentHotelId = this._id;
+      Hotels.remove(currentHotelId);
+      Router.go('hotelsList');
+    }
+  }
+});
