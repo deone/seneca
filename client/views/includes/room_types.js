7d7bc4ab07623180e8fa6ab93fc194ece667a85c
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-04-10 00:07:14 +0100

    Add room type. Validate all fields before saving.

--- client/views/includes/room_types.js
+++ client/views/includes/room_types.js
@@ -1,26 +1,46 @@
+var validate = function(e) {
+  value = $(e.target).val();
+
+  if (!value)  {
+    throwError("Please enter room " + e.target.name);
+    e.target.focus();
+  }
+
+  clearErrors();
+
+}
+
 Template.roomTypes.events({
-  'change input[name=type]': function(e)  {
-
-    $(".alert").fadeIn();
-     
+  'blur input[name=type]': validate,
+  'blur textarea[name=details]': validate,
+  'change input[name=price]': function(e) {
     var currentHotelId = this._id;
-    var room = {type: $(e.target).val()};
     var hotel = Hotels.findOne(currentHotelId);
+    var room = {
+      type: $('input[name=type]').val(),
+      details: $('textarea[name=details]').val(),
+      price: $(e.target).val()
+    }
 
-    if (hotel.rooms) {
-      Hotels.update(currentHotelId, {$push: {rooms: room}}, function(error)  {
-        if(error)
-          throwError(error.reason);
-      });
+    if (!room.type || !room.details || !room.price)  {
+      throwError("All fields are mandatory");
     } else  {
-      Hotels.update(currentHotelId, {$set: {rooms: [room]}}, function(error)  {
-        if(error)
-          throwError(error.reason);
-      });
+      $(".alert").fadeIn();
+      if (hotel.rooms) {
+        Hotels.update(currentHotelId, {$push: {rooms: room}}, function(error)  {
+          if(error)
+            throwError(error.reason);
+        });
+      } else  {
+        Hotels.update(currentHotelId, {$set: {rooms: [room]}}, function(error)  {
+          if(error)
+            throwError(error.reason);
+        });
+      }
     }
 
     $(".alert").html("<strong>Saved!</strong>");
-  },
+  }
 });
 
 Template.roomTypes.rendered = function() {
