afe223a1c8255c9fd4e1c59aaca32a229a32f830
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-04-12 18:35:37 +0100

    Now that we've successfully added rooms as a nested collection in hotels, we need to move it into it's own collection.

--- client/views/includes/room_types.js
+++ client/views/includes/room_types.js
@@ -1,46 +1,28 @@
-var validate = function(e) {
-  value = $(e.target).val();
-
-  if (!value)  {
-    throwError("Please enter room " + e.target.name);
-    e.target.focus();
-  }
-
-  clearErrors();
-
-}
-
 Template.roomTypes.events({
-  'blur input[name=type]': validate,
-  'blur textarea[name=details]': validate,
-  'change input[name=price]': function(e) {
+  'blur form': function(e) {
+
     var currentHotelId = this._id;
     var hotel = Hotels.findOne(currentHotelId);
-    var room = {
-      type: $('input[name=type]').val(),
-      details: $('textarea[name=details]').val(),
-      price: $(e.target).val()
-    }
 
-    if (!room.type || !room.details || !room.price)  {
-      throwError("All fields are mandatory");
-    } else  {
-      $(".alert").fadeIn();
-      if (hotel.rooms) {
-        Hotels.update(currentHotelId, {$push: {rooms: room}}, function(error)  {
-          if(error)
-            throwError(error.reason);
-        });
-      } else  {
-        Hotels.update(currentHotelId, {$set: {rooms: [room]}}, function(error)  {
-          if(error)
-            throwError(error.reason);
-        });
-      }
-    }
+    var type = $('input[name=type]').val(), 
+      summary = $('textarea[name=summary]').val(), 
+      price = $('input[name=price]').val();
 
-    $(".alert").html("<strong>Saved!</strong>").fadeOut(2000);
+    var rooms = hotel.rooms;
+    if (!rooms)
+      var rooms = {};
+
+    rooms[type] = {summary: summary, price: price};
+
+    $(".alert-success").fadeIn();
+    Hotels.update(currentHotelId, {$set: {rooms: rooms}}, function(error)  {
+      if(error)
+        throwError(error.reason);
+    });
+    $(".alert-success").html("<strong>Saved!</strong>").fadeOut(5000);
+
   }
+
 });
 
 Template.roomTypes.rendered = function() {
