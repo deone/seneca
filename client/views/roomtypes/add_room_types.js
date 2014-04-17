6c8b5546f65d34603281d0c6812e5762f2fc8c53
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-04-17 15:27:47 +0100

    Validate type field, show success feedback and call method addRoomType()

--- client/views/roomtypes/add_room_types.js
+++ client/views/roomtypes/add_room_types.js
@@ -1,8 +1,52 @@
+showSuccessFeedback = function(e) {
+  $(e.target).next('.form-control-feedback').remove();
+  $(e.target).parents('.form-group').removeClass('has-error').addClass('has-success');
+  $(e.target).after('<span class="glyphicon glyphicon-ok form-control-feedback"></span>');
+}
+
+showErrorFeedback = function(elem)  {
+  elem.next('.form-control-feedback').remove();
+  elem.parents('.form-group').removeClass('has-success').addClass('has-error');
+  elem.after('<span class="glyphicon glyphicon-remove form-control-feedback"></span>');
+}
+
 Template.addRoomTypes.events({
+  'change input[name=type]': showSuccessFeedback,
   'blur form': function(e) {
 
     var currentHotelId = this._id;
-    var hotel = Hotels.findOne(currentHotelId);
+
+    var typeEl = $('input[name=type]'), 
+      summaryEl = $('textarea[name=summary]'),
+      priceEl = $('input[name=price]');
+
+    var roomType = {
+      hotelId: currentHotelId,
+      type: typeEl.val(), 
+      summary: summaryEl.val(), 
+      price: priceEl.val()
+    };
+
+    if (!roomType.type || roomType.type === " ") {
+      showErrorFeedback(typeEl);
+      typeEl.focus();
+      return;
+    }
+
+    $(".alert-success").fadeIn();
+    Meteor.call('addRoomType', roomType, function(error, result)  {
+      if (error)  {
+        throwError(error.reason);
+      } else  {
+        /* typeEl.val('');
+        summaryEl.val('');
+        priceEl.val(''); */
+        $(".alert-success").html("<strong>Saved!</strong>").fadeOut(3000);
+      }
+
+    });
+
+    /* var hotel = Hotels.findOne(currentHotelId);
 
     var type = $('input[name=type]').val(), 
       summary = $('textarea[name=summary]').val(), 
@@ -19,7 +63,7 @@ Template.addRoomTypes.events({
       if(error)
         throwError(error.reason);
     });
-    $(".alert-success").html("<strong>Saved!</strong>").fadeOut(5000);
+    $(".alert-success").html("<strong>Saved!</strong>").fadeOut(5000); */
 
   }
 
