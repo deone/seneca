a767f90a63e9a2e9f356e3a1f2bc4b4e3b54b082
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-04-17 14:29:51 +0100

    Add and update room types without duplicates.

--- collections/room_types.js
+++ collections/room_types.js
@@ -1,4 +1,4 @@
-RoomTypes = new Meteor.Collection('roomTypes');
+RoomTypes = new Meteor.Collection('roomtypes');
 
 RoomTypes.allow({
   insert: ownsDocument,
@@ -7,6 +7,34 @@ RoomTypes.allow({
 });
 
 Meteor.methods({
-  addRoom: function(roomTypeAttributes) {
+  addRoomType: function(roomTypeAttributes) {
+
+    var user = Meteor.user();
+    
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to add room types");
+
+    var roomType = RoomTypes.findOne({type: roomTypeAttributes.type});
+
+    if (!roomType)  {
+      roomType = _.extend(roomTypeAttributes, {
+        userId: user._id,
+        added: new Date().getTime()
+      });
+
+      var roomTypeId = RoomTypes.insert(roomType);
+
+      return roomTypeId;
+    } else  {
+      RoomTypes.update(roomType._id, 
+          {$set: {
+                  type: roomTypeAttributes.type,
+                  summary: roomTypeAttributes.summary,
+                  price: roomTypeAttributes.price
+                }
+          }
+      );
+    }
+
   }
 });
