351341aff6d21a173895bc237661d83a540e5561
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-05-15 17:49:12 +0100

    Stack google places autocomplete dropdown on navbar-static-side instead of below.

--- client/views/includes/header.js
+++ client/views/includes/header.js
@@ -5,3 +5,14 @@ Template.header.rendered = function() {
   var input = document.getElementById('where-header');
   new google.maps.places.Autocomplete(input, options);
 };
+
+Template.header.events({
+  'keypress #where-header': function()  {
+    var pacContainerInitialized = false;
+
+    if (!pacContainerInitialized) {
+      $('.pac-container').css('z-index', '9999');
+      pacContainerInitialized = true;
+    }
+  }
+});
