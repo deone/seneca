51f05ff7f1a5e62ec6a052e9827bd035ee4fb81a
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-05-16 17:59:05 +0100

    Remove obsolete files.

--- client/views/hotels/edit_hotel_nav.js
+++
@@ -1,7 +0,0 @@
-Template.editHotelNav.events({
-  'click .nav-item': function(e)  {
-    e.preventDefault();
-    Session.set('template', e.target.id);
-    window.history.pushState(null, null, '/edit/' + this._id + '/' + e.target.id);
-  }
-});
