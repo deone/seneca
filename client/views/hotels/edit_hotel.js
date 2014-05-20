581afba0a6c5a3379c17084573c573070b94786d
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-05-20 12:54:20 +0100

    Let jQuery animate scroll fully; remove '- 30'. Implement Back to Top.

--- client/views/hotels/edit_hotel.js
+++ client/views/hotels/edit_hotel.js
@@ -38,7 +38,7 @@ Template.editHotel.events({
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top - 30
+          scrollTop: target.offset().top
         }, 1000);
         return false;
       }
