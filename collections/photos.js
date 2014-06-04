ac5f9ea3d1a466f0ce007208506333dea1ed6225
Oladayo Osikoya <alwaysdeone@gmail.com>
2014-06-04 13:16:40 +0100

    Upload photos to Amazon S3.

--- collections/photos.js
+++ collections/photos.js
@@ -1,25 +1,42 @@
 var thumbnailFormat = {width: 100, height: 100};
 
-var fullPhotoStore = new FS.Store.FileSystem('photo', {path: '~/uploads/photos'});
+var thumbTransform = function(file, readStream, writeStream) {
+  gm(readStream, file.name)
+    .resize(thumbnailFormat.width, thumbnailFormat.height)
+    .stream(function (err, stdout, stderr)  {
+    if (err) {
+      console.error(err.toString());
+    } else {
+      stderr.on('data', function (err) {
+        console.error(err.toString());
+      });
+      stdout.pipe(writeStream);
+    }
+  });
+  // readStream.pipe(writeStream);
+}
+
+// cfs-filesystem
+//
+/* var fullPhotoStore = new FS.Store.FileSystem('photo', {path: '~/uploads/photos'});
 var thumbnailStore = new FS.Store.FileSystem('thumb', {
   path: '~/uploads/thumbs',
-  transformWrite: function(file, readStream, writeStream) {
-    gm(readStream, file.name).resize(thumbnailFormat.width, thumbnailFormat.height).stream(function (err, stdout, stderr)  {
-      if (err) {
-        console.error(err.toString());
-      } else {
-        stderr.on('data', function (err) {
-          console.error(err.toString());
-        });
-        stdout.pipe(writeStream);
-      }
-    });
-    // readStream.pipe(writeStream);
-  }
-});
+  transformWrite: thumbTransform
+}); */
+
+// cfs-s3
+var s3options = {
+  region: "us-west-2",
+  accessKeyId: "AKIAI2MDBBG53UFYCBGQ", //required if environment variables are not set
+  secretAccessKey: "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", //required if environment variables are not set
+  bucket: "seneca", //required
+}
+
+var photoStore = new FS.Store.S3("full", _.extend({}, s3options, {folder: 'full/'}));
+var thumbStore = new FS.Store.S3("thumb", _.extend({}, s3options, {folder: 'thumbs/', transformWrite: thumbTransform}));
 
 Photos = new FS.Collection("photos", {
-  stores: [fullPhotoStore, thumbnailStore],
+  stores: [photoStore, thumbStore],
   filter: {
     allow: {
       contentTypes: ['image/*'],
